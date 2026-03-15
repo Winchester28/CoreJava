12e34acaee79fadbfdb59311b389f16e7c20f4b8
Ashirwad Swain <ashirbadswain77@gmail.com>
2026-03-15 22:00:36 +0530

    ArrayDemo1

---
+++ src/com/ashirwad/array/ArrayInitialization.java
@@ -0,0 +1,15 @@
+package com.ashirwad.array;
+
+import java.util.Arrays;
+
+public class ArrayInitialization {
+int [] arr= new int[5];
+{
+	arr [0]=10;
+}
+
+void main()
+{
+	IO.println(Arrays.toString(arr));
+	}
+}
